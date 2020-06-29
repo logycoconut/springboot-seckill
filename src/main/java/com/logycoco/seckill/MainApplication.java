e7ba557ca21dc6d231f04f3d6f9f762091e4326b
logycoconut <1425795337@qq.com>
2020-06-29 22:47:19 +0800

    项目初始化

---
+++ src/main/java/com/logycoco/seckill/MainApplication.java
@@ -0,0 +1,15 @@
+package com.logycoco.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hall
+ * @date 2020-06-23 00:18
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
