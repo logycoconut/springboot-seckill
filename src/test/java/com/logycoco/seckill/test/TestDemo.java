fddecd2bac51804cbba8db4f9fa7ed4939ff6edc
logycoconut <1425795337@qq.com>
2020-07-01 23:41:21 +0800

    test

---
+++ src/test/java/com/logycoco/seckill/test/TestDemo.java
@@ -0,0 +1,40 @@
+package com.logycoco.seckill.test;
+
+import com.logycoco.seckill.MainApplication;
+import com.logycoco.seckill.enity.User;
+import com.logycoco.seckill.mapper.UserMapper;
+import com.logycoco.seckill.utils.RSAUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+/**
+ * @author hall
+ * @date 2020-06-29 23:17
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+public class TestDemo {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        List<User> users = this.userMapper.selectList(null);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void generateKeys() throws IOException, NoSuchAlgorithmException {
+        String pubKeyPath = "/Users/hall/Documents/WorkSpace/seckill/src/main/resources/rsa/rsa.pub";
+        String priKeyPath = "/Users/hall/Documents/WorkSpace/seckill/src/main/resources/rsa/rsa.pri";
+
+        RSAUtils.generateKey(pubKeyPath, priKeyPath, "123456");
+    }
+}
